44b3dd73728e432e4eda461bfc2588d480e4db79
Iam-VM <vishnumohanantheking@gmail.com>
2020-04-09 00:45:28 +0530

    committing core code

--- test.js
+++ test.js
@@ -1,14 +1,30 @@
-const git = require("simple-git");
+const git = require("simple-git/promise")();
 const moment = require("moment");
 const jsonfile = require("jsonfile");
 const crypto = require("crypto");
 const random = require("random");
 
-const PATH = "./gibberish.json";
-const initial_date = moment().subtract(1, 'y'); 
-const final_date = moment().format();
 
-jsonfile.writeFile(PATH, {"secrete_key" : crypto.randomBytes(32).toString("hex")});
+let initial_date = moment().subtract(1, 'y'); 
+const final_date = moment().format('MMMM Do YYYY');
 
-git().add(".").commit("test commit", {"--date" : initial_date.format()}).push();
-console.log("Commited");
+
+
+const committing = async (initial_date, final_date) => {
+    if (initial_date.format('MMMM Do YYYY') === final_date){
+        console.log("\n\n----End of process----");
+        return;
+    }
+    else{
+        jsonfile.writeFile("./gibberish.json", {"secrete_key" : crypto.randomBytes(32).toString("hex")});
+        await git.add("./gibberish.json");
+        await git.commit("commit by bot", {"--date" : initial_date.format()});
+        await git.push(['-u', 'origin' ,'master']);
+        console.log(`Pushing Complete for ${initial_date.format('MMMM Do YYYY')}`);
+        await committing(initial_date.add(1, 'days'), final_date);
+        return;
+    }
+};
+
+
+committing(initial_date, final_date);
